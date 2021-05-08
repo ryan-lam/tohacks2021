5a44a68bb8678fd9d35a1784e4b08f85fc1c002a
ryan-lam <ryan-lam@users.noreply.github.com>
2021-05-08 16:10:37 -0400

    working login page

--- myinsurance/main/views.py
+++ myinsurance/main/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 
 # Create your views here.
 
+# Create the login form
+class Login(forms.Form):
+    username = forms.CharField(label="Username")
+    password = forms.CharField(widget=forms.PasswordInput)
 
+
+
+# Login page (FULLY WORKS)
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        login = Login(request.POST)
+
+        if login.is_valid():
+            username =  login.cleaned_data["username"]
+            password =  login.cleaned_data["password"]
+
+            if username == "admin" and password == "1234":
+                return render(request, "testadmin.html")
+            else:
+                return render (request, "index.html", {
+                    "login":Login(), "attempt":True
+                })
+
+    return render(request, "index.html", {
+        "login": Login(), "attempt":False
+    })
+
